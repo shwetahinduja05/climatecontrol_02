b4da6ebfb47e911a334df883e726b2c15532d626
shhinduj <shhinduj@tibco.com>
2019-10-05 12:28:32 +0530

    commit all files

---
+++ src/main/java/com/shweta/climatecontrol_02/Climatecontrol02Application.java
@@ -0,0 +1,13 @@
+package com.shweta.climatecontrol_02;
+
+import org.springframework.boot.SpringApplication;
+import org.springframework.boot.autoconfigure.SpringBootApplication;
+
+@SpringBootApplication
+public class Climatecontrol02Application {
+
+	public static void main(String[] args) {
+		SpringApplication.run(Climatecontrol02Application.class, args);
+	}
+
+}
